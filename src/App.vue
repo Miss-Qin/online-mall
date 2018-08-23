3af882848b0021d1b90239027664234f7af949a7
Qin <13827194007@139.com>
2018-08-23 10:51:34 +0800

    增加页面的头尾

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <common></common>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import common from './components/common/common.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    common
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
